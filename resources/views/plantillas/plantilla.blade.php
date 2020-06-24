971887df3173ae3c7961208ead91cf1da7a55ce1
jesus hernan <jesushernan@gmail.com>
2020-06-24 14:01:40 -0300

    modificaiones a swiper - desde Minilucille

--- resources/views/plantillas/plantilla.blade.php
+++ resources/views/plantillas/plantilla.blade.php
@@ -1,54 +1,56 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>AdT</title>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
+  <title>AdT</title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="/css/bulma.css">
-        <link rel="stylesheet" href="/css/main.css">
-        <link rel="stylesheet" href="/css/LineIcons.css">
-        <link rel="stylesheet" href="/css/line-awesome.min.css">
+  <!-- Fonts -->
 
-        <link rel="stylesheet" href="/css/swiper.css">
-        <link rel="stylesheet" href="/css/swiper.min.css">
+  <!-- Styles -->
+  <link rel="stylesheet" href="/css/bulma.css">
+  <link rel="stylesheet" href="/css/main.css">
+  <link rel="stylesheet" href="/css/LineIcons.css">
+  <link rel="stylesheet" href="/css/line-awesome.min.css">
+
+  <link rel="stylesheet" href="/css/swiper.css">
+  <link rel="stylesheet" href="/css/swiper.min.css">
 
 
-    </head>
-    <body>
-      @include('plantillas.parciales.header')
-      @yield('contenido')
-      @include('plantillas.parciales.footer')
-    
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="/js/main.js"></script>
-    <script src="/js/swiper.js"></script>
-    <script src="/js/swiper.min.js"></script>
-    <script>
-      var mySwiper = new Swiper ('.swiper-container', {
-        // Optional parameters
+</head>
 
-        // If we need pagination
-        pagination: {
-          el: '.swiper-pagination',
-        },
+<body>
+  @include('plantillas.parciales.header')
+  @yield('contenido')
+  @include('plantillas.parciales.footer')
 
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="/js/main.js"></script>
+  <script src="/js/swiper.js"></script>
+  <script src="/js/swiper.min.js"></script>
+  <script>
+  var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
 
-        // And if we need scrollbar
-        scrollbar: {
-          el: '.swiper-scrollbar',
-        },
-      })
-    </script>
-    </body>
-    </html>
-    
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  })
+  </script>
+</body>
+
+</html>
